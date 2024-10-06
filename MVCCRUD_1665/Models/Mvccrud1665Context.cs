ebd32b0b8b55bc21ead212ff05f9fccb691af96f
Angelismejia <Angelismejia06@gmail.com>
2024-10-05 23:18:33 -0400

    Add project files.

---
+++ MVCCRUD_1665/Models/Mvccrud1665Context.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCCRUD_1665.Models;
+
+public partial class Mvccrud1665Context : DbContext
+{
+    public Mvccrud1665Context()
+    {
+    }
+
+    public Mvccrud1665Context(DbContextOptions<Mvccrud1665Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            
+        }
+    }
+
+    ///#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    /// => optionsBuilder.UseSqlServer("server=localhost; database=MVCCRUD_1665; integrated security=true; TrustServerCertificate=true;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Usuarios__3213E83F61B86E17");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Clave)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
